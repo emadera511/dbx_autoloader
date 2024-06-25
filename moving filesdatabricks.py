636f2567abe12675c5cf2f008f9c6455abe5f35a
emadera80 <emadera@maderaanalytics.com>
2024-06-25 11:05:20 -0400

    added sql

--- moving filesdatabricks.py
+++ moving filesdatabricks.py
@@ -1,11 +1,17 @@
-source_path = 's3://somelocation/location/dt=2024-05-14'
-destination_path = 's3://somelocation/location/dt=2024-05-24'
+def move_files_dbx(source_path, destination_path): 
+
+    ls = [] 
+
+    for dir_path in dbutils.fs.ls(source_path): 
+        file_name = dir_path.name 
+        ls.append(file_name) 
+        dest = f'{destination_path}{file_name}'
+        dbutils.fs.cp(dir_path.path, dest, recurse = True)
 
 
-ls = [] 
+date_list = ['2024-06-10', '2024-06-12', '2024-06-14']
 
-for dir_path in dbutils.fs.ls(source_path): 
-    file_name = dir_path.name 
-    ls.append(file_name) 
-    dest = f'{destination_path}{file_name}'
-    dbutils.fs.cp(dir_path.path, dest, recurse = True)
+
+for date in date_list: 
+    source_path = 's3://somelocation/location/dt={date}'
+    destination_path = 's3://somelocation/location/dt=2024-06-24'
